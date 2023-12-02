dffc5481b07e44ae481301cf6e742daf21a91fc6
Amcalstrings <128369881+Amcalstrings@users.noreply.github.com>
2023-12-02 03:50:32 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,19 @@
 from turtle import Turtle, Screen
-
+"""creeating our object from our Turtle class"""
 tim = Turtle()
-"""The following lines of code takes user inputs"""
+"""creeating our object from our Screen class"""
 screen = Screen()
-screen.listen()  # call the listen function
+screen.listen()  # listen for user inputs
 
 # def move_forward():
 #     return tim.forward(10)
 #
 #
-# screen.onkey(key="space", fun=move_forward) #the key activates the function we have defined inside the function(screen.onkey)
+# screen.onkey(key="space", fun=move_forward) # the key keyword argument assigns a button to the function we have defined.
 #
 # screen.exitonclick()
 
-
+""" Defining how our turtle object moves below """
 def move_forward():
     return tim.forward(10)
 
@@ -52,7 +52,7 @@ def turn_right():
     tim.heading()
     new_heading = tim.setheading(tim.heading() - 45)
 
-
+"""assigning butttons on our keyboard to the functions defined above"""
 screen.onkey(key="d", fun=move_clockwise)
 screen.onkey(key="a", fun=move_counter_clockwise)
 screen.onkey(key="w", fun=move_forward)
